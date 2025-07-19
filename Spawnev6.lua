afa4f06d905d6f33b91bcc4a8945b872af78f53b
Petspawner1 <babekosija@gmail.com>
2025-07-19 10:29:18 +0800

    Update Spawnev6.lua

--- Spawnev6.lua
+++ Spawnev6.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/Mi1xvxYupi/0", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Petspawner1/Antipatchx/refs/heads/main/ea58e5676e007216532f7a03d8521244.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Petspawner1/newbypass/refs/heads/main/Spawnev6.lua"))()
